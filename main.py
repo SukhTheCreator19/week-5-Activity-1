928be4570be53f8c31ea6e92c726a9ee64b1fb1d
SukhTheCreator19 <143115777+SukhTheCreator19@users.noreply.github.com>
2023-09-20 17:52:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from learn_dictionaries import *
+from learn_tuples import *
+from learn_sets import *
+from learn_booleans import *
+
+learn_dictionaries()
+learn_tuples()
+learn_sets()
+learn_booleans()
+
