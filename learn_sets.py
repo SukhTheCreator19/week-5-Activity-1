5a16b434030491ab6479977554d37b0438cfa10e
SukhTheCreator19 <143115777+SukhTheCreator19@users.noreply.github.com>
2023-09-25 17:48:16 +0000

    dsfvs

--- learn_sets.py
+++ learn_sets.py
@@ -1,18 +1,22 @@
 def learn_sets():
   print("I am in sets")
    ###############################SETS########################################
-  # Sets Practice #1
+ # Sets Practice #1
   # Join the following sets into one, called my_set_3:
-  # {1, 2, "three", "four"}
-  # {"three", 4, 5}
-  # my_set_1 = {1, 2, "three", "four"}
+  my_set = {1, 2, "three", "four"}
+  my_set1 = {"three", 4, 5}
+  my_set2 = {1, 2, "three", "four"}
+  my_set0 = {"three", 4, 5}
   
-  # my_set_2 = {"three", 4, 5}
+  my_set3 = {f"{my_set},{my_set1}, {my_set2}, {my_set0}"}
+  print(my_set3)
+    # Sets Practice #2
+    # Remove a random item from the following set, using set methods. 
+  raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
+  raffle.remove("Rachel")
   
-  # Sets Practice #2
-  # Remove a random item from the following set, using set methods.
-  # raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
-  
-  # Sets Practice #3
-  # Add the name Gunther to the following set, using set methods:
-  # raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
+    # Sets Practice #3
+    # Add the name Gunther to the following set, using set methods:
+    # 
+  raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
+  raffle.add("Gunther")
--- learn_tuples.py
+++ learn_tuples.py
@@ -5,16 +5,21 @@ def learn_tuples():
   # Tuples Practice #1
   # Use a tuple method to count the number of times the value 2 appears in the following tuple, and display the result (integer) on the screen:
   
-  # my_tuple = (1, 2, 3, 2, 3, 1, 3, 2, 3, 3, 3, 1, 3, 2, 2, 1, 3, 2)
-  
+  my_tuple = (1, 2, 3, 2, 3, 1, 3, 2, 3, 3, 3, 1, 3, 2, 2, 1, 3, 2)
+  print(my_tuple.count(2))
   # Tuples Practice #2
   # Convert the following tuple to a list, and store it in a variable called my_list.
   
-  # my_tuple = (1, 2, 3, 2, 3, 1, 3, 2)
-  
+  my_tuple1 = [1, 2, 3, 2, 3, 1, 3, 2]
+  my_list = my_tuple1
+  print(my_tuple1)
   # Tuples Practice #3
   # Extract the elements of the following tuple into four variables: a, b, c, d
   
-  # my_tuple = (1, 2, 3, 4)
-  
+  my_tuple2 = (1, 2, 3, 4)
+  a, b, c, d = my_tuple2
+  print(a)
+  print(b)
+  print(c)
+  print(d)
 
